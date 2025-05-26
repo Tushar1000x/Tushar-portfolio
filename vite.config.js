3c0c5e61652b57baa6a6991630c37014582a2c9d
Tushar <singhtushar1000x@gmail.com>
2025-05-26 15:49:09 +0530

    using mains deploy only

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/Tushar-portfolio/', // Replace with your actual repo name
+//   build: {
+//     outDir: 'dist' // Ensure build outputs to 'dist' folder
+//   }
+// })
+// vite.config.js
 export default defineConfig({
-  plugins: [react()],
-  base: '/Tushar-portfolio/', // Replace with your actual repo name
-  build: {
-    outDir: 'dist' // Ensure build outputs to 'dist' folder
-  }
+  base: '/', // Instead of repo name
 })
