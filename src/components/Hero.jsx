f8ac4e030cffaf7508eaf6a07cd7529dad210a39
Tushar <singhtushar1000x@gmail.com>
2025-05-26 18:27:16 +0530

    abc

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-   const isMobile = window.innerWidth <= 700;
+  //  const isMobile = window.innerWidth <= 700;
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
@@ -27,7 +27,7 @@ const Hero = () => {
         </div>
       </div>
 
- {!isMobile && <ComputersCanvas />}
+  <ComputersCanvas />
      
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
