08b2a6c34f11e6836669414a2668c6385d1d6006
Tushar <singhtushar1000x@gmail.com>
2025-05-26 15:56:48 +0530

    hashrouter

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/Tushar-portfolio/', // Replace with your actual repo name
-//   build: {
-//     outDir: 'dist' // Ensure build outputs to 'dist' folder
-//   }
-// })
-// vite.config.js
 export default defineConfig({
-  base: '/', // Instead of repo name
+  plugins: [react()],
+  base: '/Tushar-portfolio/', // Replace with your actual repo name
+  build: {
+    outDir: 'dist' // Ensure build outputs to 'dist' folder
+  }
 })
