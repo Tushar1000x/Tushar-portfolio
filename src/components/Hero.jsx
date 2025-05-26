87fbaceefec078e32d8a76fafa3b53b105df57cf
Tushar <singhtushar1000x@gmail.com>
2025-05-26 18:04:57 +0530

    mobile screen width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+   const isMobile = window.innerWidth <= 700;
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
@@ -26,7 +27,8 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+ {!isMobile && <ComputersCanvas />}
+     
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
